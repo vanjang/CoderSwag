acdf4125859148f18359314f0a8b35f87bc655c9
Myung Hoon So <joa214@hotmail.com>
2020-06-10 20:41:32 +0100

    custom adapter

--- app/src/main/java/com/cochipcho/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/cochipcho/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.cochipcho.coderswag.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.cochipcho.coderswag.Adapters.CategoryAdapter
 import com.cochipcho.coderswag.Model.Category
 import com.cochipcho.coderswag.R
 import com.cochipcho.coderswag.Services.DataServices
@@ -10,13 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataServices.categories)
+        adapter = CategoryAdapter(this, DataServices.categories)//ArrayAdapter(this, android.R.layout.simple_list_item_1, DataServices.categories)
         categoryListView.adapter = adapter
     }
 }
